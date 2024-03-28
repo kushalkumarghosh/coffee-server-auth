4472feae2210189d7a8b2ef790cf7a0aa29174e9
kushalkumarghosh <cse.kushal1@gmail.com>
2024-03-28 21:14:51 +0600

    patch operation

--- index.js
+++ index.js
@@ -90,6 +90,19 @@ async function run() {
       res.send(result);
     });
 
+    app.patch('/user', async(req, res)=>{
+      const user = req.body;
+      const filter = {email: user.email}
+      const updatedDoc = {
+        $set:{
+          lastLoggedAt: user.lastLoggedAt
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+
+    })
+
     app.delete("/user/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
