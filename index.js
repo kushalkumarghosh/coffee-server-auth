bb782938dd403dc315a3d7a44009780b4be039fd
kushalkumarghosh <cse.kushal1@gmail.com>
2024-03-28 17:38:52 +0600

    create user to the database

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ async function run() {
     await client.connect();
 
     const coffeeCollection = client.db("coffeeDB").collection("coffee");
+    const userCollection = client.db('coffeeDB').collection('user');
 
     app.get("/coffee", async (req, res) => {
       const cursor = coffeeCollection.find();
@@ -73,6 +74,14 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //user related apis
+    app.post('/user', async(req, res)=>{
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
       res.send(result);
     });
 
